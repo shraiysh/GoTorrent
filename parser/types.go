73b541e5c8f6e7fe9e9e381cca4edeb487ce22c3
Sai Ramana Reddy <35768756+sairamanareddy@users.noreply.github.com>
2019-01-22 21:48:51 +0530

    Fix #2 : Author@sairamanareddy

--- parser/types.go
+++ parser/types.go
@@ -1,44 +1,50 @@
 package parser
 
 import (
-			"time"
-			"github.com/zeebo/bencode"
+	"time"
+
+	"github.com/zeebo/bencode"
 )
+//Struct Tags help bencode in identifying which fields to fill.
 
+//FileMetaData contains MetaData about a File.
 type FileMetaData struct {
-	Path []string `bencode:"path"`
-	Length int64 `bencode:"length"`
+	Path   []string `bencode:"path"`
+	Length int64    `bencode:"length"`
 }
 
+//InfoMetaData contains MetaData about the torrent.
 type InfoMetaData struct {
-	PieceLength int64 `bencode:"piece length"`
-	Pieces []byte `bencode:"pieces"`
-
-	Name string `bencode:"name"`
-	Length int64 `bencode:"length"`
-
-	Files bencode.RawMessage `bencode:"files"`
+	PieceLength int64              `bencode:"piece length"`
+	Pieces      []byte             `bencode:"pieces"`
+	Name        string             `bencode:"name"`
+	Length      int64              `bencode:"length"`
+	Files       bencode.RawMessage `bencode:"files"`
 }
 
+//MetaData contains MetaData about the file.
 type MetaData struct {
-	Announce     string `bencode:"announce"`
-	AnnounceList [][]string `bencode:"announce-list"`
-	Comment      string `bencode:"comment"`
-	CreatedBy    string `bencode:"created by"`
-	CreatedAt    int64 `bencode:"creation date"`
-	Info bencode.RawMessage `bencode:"info"`
+	Announce     string             `bencode:"announce"`
+	AnnounceList [][]string         `bencode:"announce-list"`
+	Comment      string             `bencode:"comment"`
+	CreatedBy    string             `bencode:"created by"`
+	CreatedAt    int64              `bencode:"creation date"`
+	Info         bencode.RawMessage `bencode:"info"`
 }
 
+//File contains length and path of a File in the torrent.
 type File struct {
-	Path []string
+	Path   []string
 	Length int64
 }
 
+//TorrentFile contains information about the torrent.
 type TorrentFile struct {
-	Announce []string
-	Comment string
+	Announce  []string
+	Comment   string
 	CreatedBy string
 	CreatedAt time.Time
-	InfoHash string
-	Files []*File
+	InfoHash  string
+	Length    int64
+	Files     []*File
 }
