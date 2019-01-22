81416cc6aea493f6a14c83b05afed50ba1441743
Sai Ramana Reddy <35768756+sairamanareddy@users.noreply.github.com>
2019-01-22 21:48:51 +0530

    Fix #2 : Author@sairamanareddy

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import ( 
 			"fmt"
 			"./parser"
-			"./tracker"
 		)
 
 func main() {
-	torrent_file,_ := parser.ParseFromFile("./test_torrents/[TorrentCounter.to].Thugs.of.Hindostan.2018.Hindi.720p.BluRay.x264.[1.2GB].[MP4].torrent")
-	fmt.Println(torrent_file)
+	torrentfile,_ := parser.ParseFromFile("./test_torrents/[TorrentCounter.to].Thugs.of.Hindostan.2018.Hindi.720p.BluRay.x264.[1.2GB].[MP4].torrent")
+	for _, A := range torrentfile.Files{
+		fmt.Println(A.Path)
+	}
+	fmt.Println(torrentfile.Length)
+	fmt.Println(torrentfile.InfoHash)
 
 }
--- parser/parser.go
+++ parser/parser.go
@@ -5,28 +5,33 @@ import (
 	"io/ioutil"
 	"os"
 	"time"
-	"github.com/zeebo/bencode"
+
+	bencode "github.com/zeebo/bencode"
 )
 
-func Parse(reader io.Reader) (*TorrentFile, error) {
+//Parse parses from a stream and returns a pointer to a TorrentFile.
+func Parse(reader io.Reader) (TorrentFile, error) {
 	data, err := ioutil.ReadAll(reader)
-
+	//return an error if reading fails.
 	if err != nil {
-		return nil, err
+		return TorrentFile{}, err
 	}
 
 	metadata := &MetaData{}
 	err = bencode.DecodeBytes(data, metadata)
+	//return an error if decode fails.
 	if err != nil {
-		return nil, err
+		return TorrentFile{}, err
 	}
 
 	info := &InfoMetaData{}
 	err = bencode.DecodeBytes(metadata.Info, info)
+	//return an error if further decode fails.
 	if err != nil {
-		return nil, err
+		return TorrentFile{}, err
 	}
-
+	//This variable refers to the Total torrent size.
+	var Length int64
 	files := make([]*File, 0)
 	// single file context
 	if info.Length > 0 {
@@ -34,11 +39,13 @@ func Parse(reader io.Reader) (*TorrentFile, error) {
 			Path:   []string{info.Name},
 			Length: info.Length,
 		})
+		Length = info.Length
 	} else {
+		//multiple files are present.
 		metadataFiles := make([]*FileMetaData, 0)
 		err = bencode.DecodeBytes(info.Files, &metadataFiles)
 		if err != nil {
-			return nil, err
+			return TorrentFile{}, err
 		}
 
 		for _, f := range metadataFiles {
@@ -46,9 +53,11 @@ func Parse(reader io.Reader) (*TorrentFile, error) {
 				Path:   append([]string{info.Name}, f.Path...),
 				Length: f.Length,
 			})
+			Length += f.Length
 		}
 	}
 
+	//announces is the list of trackers.
 	announces := make([]string, 0)
 
 	if len(metadata.AnnounceList) > 0 {
@@ -60,22 +69,26 @@ func Parse(reader io.Reader) (*TorrentFile, error) {
 	} else {
 		announces = append(announces, metadata.Announce)
 	}
-	
-	return &TorrentFile{
+
+	//return the object containing the metadata.
+	return TorrentFile{
 		Announce:  announces,
 		Comment:   metadata.Comment,
 		CreatedBy: metadata.CreatedBy,
 		CreatedAt: time.Unix(metadata.CreatedAt, 0),
 		InfoHash:  toSHA1(metadata.Info),
+		Length:    Length,
 		Files:     files,
 	}, nil
 }
 
-func ParseFromFile(path string) (*TorrentFile, error) {
+//ParseFromFile parses a .torrent file.
+func ParseFromFile(path string) (TorrentFile, error) {
 	file, err := os.Open(path)
 	if err != nil {
-		return nil, err
+		return TorrentFile{}, err
 	}
+	//Close the file after returning automatically.
 	defer file.Close()
 
 	return Parse(file)
