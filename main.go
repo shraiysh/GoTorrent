61f4635099c9e91b3f3213433d1a127f3feb87cb
Puneet Mangla <cs17btech11029@iith.ac.in>
2019-01-25 21:17:10 +0530

    Fix #6 : build.sh and travis updated Author@Puneet2000 (#7)

--- main.go
+++ main.go
@@ -1,13 +1,13 @@
 package main
 
-import ( 
-			"fmt"
-			"./parser"
-		)
+import (
+	"./parser"
+	"fmt"
+)
 
 func main() {
-	torrentfile,_ := parser.ParseFromFile("./test_torrents/[TorrentCounter.to].Thugs.of.Hindostan.2018.Hindi.720p.BluRay.x264.[1.2GB].[MP4].torrent")
-	for _, A := range torrentfile.Files{
+	torrentfile, _ := parser.ParseFromFile("./test_torrents/[TorrentCounter.to].Thugs.of.Hindostan.2018.Hindi.720p.BluRay.x264.[1.2GB].[MP4].torrent")
+	for _, A := range torrentfile.Files {
 		fmt.Println(A.Path)
 	}
 	fmt.Println(torrentfile.Length)
