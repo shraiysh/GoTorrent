d16eff7b4a890c4cae3dc03492476eeb84240d5d
Puneet Mangla <cs17btech11029@iith.ac.in>
2019-01-25 21:17:10 +0530

    Fix #6 : build.sh and travis updated Author@Puneet2000 (#7)

--- tracker/utils.go
+++ tracker/utils.go
@@ -1,24 +1,26 @@
 package tracker
 
 import (
-	"encoding/binary"
 	"bytes"
+	"encoding/binary"
 )
 
 // BuildConnReq is the first connection request for tracker
-func BuildConnReq() ([]byte) {
-	buffer:= &bytes.Buffer{}
-	buffer.Write([]byte{0x00,0x00, 0x04, 0x17, 
-						0x27 ,0x10 ,0x19 ,0x80,
-						0x00 ,0x00 ,0x00 ,0x00,
-						0xa6 ,0xec ,0x6b,0x7d})
+func BuildConnReq() []byte {
+	buffer := &bytes.Buffer{}
+	buffer.Write([]byte{0x00, 0x00, 0x04, 0x17,
+		0x27, 0x10, 0x19, 0x80,
+		0x00, 0x00, 0x00, 0x00,
+		0xa6, 0xec, 0x6b, 0x7d})
 
 	return buffer.Bytes()
 }
 
 // ResType is for decoding the responses received from socket
 func ResType(response bytes.Buffer) string {
-	action:= binary.BigEndian.Uint32(response.Bytes())
-	if (action==0) { return "connect" }
-	return "announce" 
+	action := binary.BigEndian.Uint32(response.Bytes())
+	if action == 0 {
+		return "connect"
+	}
+	return "announce"
 }
