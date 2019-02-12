4bd04083e8a9a9a2e0ec69f9201479c1ac22b4f8
shraiysh <cs17btech11050@iith.ac.in>
2019-02-12 14:10:47 +0530

    Comment out torrent/download.go : onWholeMessage (Incorrect implementation)

--- torrent/download.go
+++ torrent/download.go
@@ -1,8 +1,8 @@
 package torrent
 
 import (
-	"bytes"
-	"encoding/binary"
+	// "bytes"
+	// "encoding/binary"
 	"net"
 )
 
@@ -10,40 +10,41 @@ type handler func([]byte)
 
 // onWholeMessage sends complete messages to callback function
 func onWholeMessage(conn *net.IPConn, msgHandler handler) { // TODO add an extra argument for callback function i.e msgHandler
-	buffer := new(bytes.Buffer)
-	handshake := true
-	resp := make([]byte, 100)
+	/*
+		buffer := new(bytes.Buffer)
+		handshake := true
+		resp := make([]byte, 100)
 
-	for {
+		for {
 
-		respLen, err := conn.Read(resp)
+			respLen, err := conn.Read(resp)
 
-		if err != nil {
-			// TODO : close the connection and return
+			if err != nil {
+				// TODO : close the connection and return
+			}
+
+			binary.Write(buffer, binary.BigEndian, resp[:respLen])
+
+			var msgLen int
+
+			if handshake {
+
+				var length uint8
+				binary.Read(buffer, binary.BigEndian, &length)
+				msgLen = int(length + 49)
+			} else {
+
+				var length int32
+				binary.Read(buffer, binary.BigEndian, &length)
+				msgLen = int(length + 4)
+			}
+
+			for len(buffer.Bytes()) >= 4 && len(buffer.Bytes()) >= msgLen {
+				// TODO implement msgHandler
+				msgHandler((buffer.Bytes())[:msgLen])
+				buffer = bytes.NewBuffer((buffer.Bytes())[msgLen:])
+				handshake = false
+			}
 		}
-
-		binary.Write(buffer, binary.BigEndian, resp[:respLen])
-
-		var msgLen int
-
-		if handshake {
-
-			var length uint8
-			binary.Read(buffer, binary.BigEndian, &length)
-			msgLen = int(length + 49)
-		} else {
-
-			var length int32
-			binary.Read(buffer, binary.BigEndian, &length)
-			msgLen = int(length + 4)
-		}
-
-		for len(buffer.Bytes()) >= 4 && len(buffer.Bytes()) >= msgLen {
-			// TODO implement msgHandler
-			msgHandler((buffer.Bytes())[:msgLen])
-			buffer = bytes.NewBuffer((buffer.Bytes())[msgLen:])
-			handshake = false
-		}
-	}
-
+	*/
 }
