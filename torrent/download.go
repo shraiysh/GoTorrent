047ad024671e71684c50341efc2b228f28117bcd
Puneet Mangla <cs17btech11029@iith.ac.in>
2019-02-13 14:34:35 +0530

    test cases for torrent/download.go : onWholeMessage (#29)
    * Fix #27 : test cases for torrent/download.go : onWholeMessage(*kwargs) author@Puneet2000
    
    * Using mock client and server connections
    
    * uint8 error resolved
    
    Minor bug fixes

--- torrent/download.go
+++ torrent/download.go
@@ -1,50 +1,44 @@
 package torrent
 
 import (
-	// "bytes"
-	// "encoding/binary"
+	"bytes"
+	"encoding/binary"
 	"net"
 )
 
 type handler func([]byte)
 
 // onWholeMessage sends complete messages to callback function
-func onWholeMessage(conn *net.IPConn, msgHandler handler) { // TODO add an extra argument for callback function i.e msgHandler
-	/*
-		buffer := new(bytes.Buffer)
-		handshake := true
-		resp := make([]byte, 100)
+func onWholeMessage(conn net.Conn, msgHandler handler) error {
+	buffer := new(bytes.Buffer)
+	handshake := true
+	resp := make([]byte, 100)
 
-		for {
+	for {
+		respLen, err := conn.Read(resp)
 
-			respLen, err := conn.Read(resp)
-
-			if err != nil {
-				// TODO : close the connection and return
-			}
-
-			binary.Write(buffer, binary.BigEndian, resp[:respLen])
-
-			var msgLen int
-
-			if handshake {
-
-				var length uint8
-				binary.Read(buffer, binary.BigEndian, &length)
-				msgLen = int(length + 49)
-			} else {
-
-				var length int32
-				binary.Read(buffer, binary.BigEndian, &length)
-				msgLen = int(length + 4)
-			}
-
-			for len(buffer.Bytes()) >= 4 && len(buffer.Bytes()) >= msgLen {
-				// TODO implement msgHandler
-				msgHandler((buffer.Bytes())[:msgLen])
-				buffer = bytes.NewBuffer((buffer.Bytes())[msgLen:])
-				handshake = false
-			}
+		if err != nil {
+			conn.Close() // TODO maybe a better implementation
+			return err
 		}
-	*/
+
+		binary.Write(buffer, binary.BigEndian, resp[:respLen])
+
+		var msgLen int
+
+		if handshake {
+			length := uint8((buffer.Bytes())[0])
+			msgLen = int(length) + 49
+		} else {
+
+			length := int32((buffer.Bytes())[0])
+			msgLen = int(length + 4)
+		}
+		for len(buffer.Bytes()) >= 4 && len(buffer.Bytes()) >= msgLen {
+			// TODO implement msgHandler
+			msgHandler((buffer.Bytes())[:msgLen])
+			buffer = bytes.NewBuffer((buffer.Bytes())[msgLen:])
+			handshake = false
+		}
+	}
 }
---
+++ torrent/download_test.go
@@ -0,0 +1,36 @@
+package torrent
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"net"
+	"testing"
+)
+
+// TestOnWholeMessage tests torrent/download.go : onWholeMessage(*kwargs)
+func TestOnWholeMessage(t *testing.T) {
+	fmt.Println("Testing torrent/download.go : onWholeMessage(*kwargs)")
+
+	//length := rand.Intn(100) // generate random handshake message
+	length := 220
+	message := make([]byte, length+49)
+	rand.Read(message)
+	message[0] = uint8(length)
+
+	client, server := net.Pipe() // create a client and server connection
+
+	go func() {
+		server.Write(message)
+		server.Close() // close after writing out all data
+	}()
+
+	err := onWholeMessage(client, func(b []byte) { // mock Message Handler
+		assert.Equal(t, len(b), int(b[0])+49, "length not equal")
+		assert.Equal(t, b, message, "message received not same")
+	})
+
+	assert.Equal(t, err, fmt.Errorf("EOF"), "Not EOF error")
+	fmt.Println("PASS")
+
+}
