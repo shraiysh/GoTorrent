32a5d23b4c8fe138b1dc160055ef3aaf9dced1a3
Sai Ramana Reddy <cs17btech11022@iith.ac.in>
2019-02-14 22:14:14 +0530

    Update download.go

--- torrent/download.go
+++ torrent/download.go
@@ -12,9 +12,11 @@ import (
 
 type handler func([]byte)
 
-// MakeHandshake is a function that handshakes with a peer specified by peer object.
+
+
+// Download is a function that handshakes with a peer specified by peer object.
 // Concurrently call this function to establish parallel connections to many peers.
-func MakeHandshake(peer tracker.Peer, report *tracker.ClientStatusReport){
+func Download(peer tracker.Peer, report *tracker.ClientStatusReport){
 	buffer, err := BuildHandshake(*report)
 	if err!=nil{
 		return
@@ -28,36 +30,35 @@ func MakeHandshake(peer tracker.Peer, report *tracker.ClientStatusReport){
 	}
 
 	fmt.Println(service)
-	//check if peer supports tcp first. anyways, if it doesn't
-	// below expression will raise an error.
 	conn, err := net.DialTCP("tcp", nil, &service)
+	conn.SetKeepAlive(true)
 	if err!=nil{
 		return
 	}
 	//write the handshake content into the connection.
 	conn.Write(buffer.Bytes())
-	//read from the connection.
+	//use onWholeMessage() to read safely from the conn.
+	//TODO: make a handler function and the paramter here.
+	onWholeMessage(conn, handler)
+}
 
-	resp:= make([]byte,68)
-	conn.Read(resp)
-	//Just to print and check stuff.
-	fmt.Println(resp)
+func msgHandler(msg []byte){
+	
+
 }
 
 // onWholeMessage sends complete messages to callback function
-func onWholeMessage(conn *net.IPConn, msgHandler handler) { // TODO add an extra argument for callback function i.e msgHandler
+func onWholeMessage(conn net.Conn, msgHandler handler) error {
 	buffer := new(bytes.Buffer)
 	handshake := true
 	resp := make([]byte, 100)
 
 	for {
-
 		respLen, err := conn.Read(resp)
 
 		if err != nil {
-			// TODO : close the connection and return
-			conn.Close()
-			return
+			conn.Close() // TODO maybe a better implementation
+			return err
 		}
 
 		binary.Write(buffer, binary.BigEndian, resp[:respLen])
@@ -65,17 +66,13 @@ func onWholeMessage(conn *net.IPConn, msgHandler handler) { // TODO add an extra
 		var msgLen int
 
 		if handshake {
-
-			var length uint8
-			binary.Read(buffer, binary.BigEndian, &length)
-			msgLen = int(length + 49)
+			length := uint8((buffer.Bytes())[0])
+			msgLen = int(length) + 49
 		} else {
 
-			var length int32
-			binary.Read(buffer, binary.BigEndian, &length)
+			length := int32((buffer.Bytes())[0])
 			msgLen = int(length + 4)
 		}
-
 		for len(buffer.Bytes()) >= 4 && len(buffer.Bytes()) >= msgLen {
 			// TODO implement msgHandler
 			msgHandler((buffer.Bytes())[:msgLen])
@@ -83,5 +80,4 @@ func onWholeMessage(conn *net.IPConn, msgHandler handler) { // TODO add an extra
 			handshake = false
 		}
 	}
-
 }
