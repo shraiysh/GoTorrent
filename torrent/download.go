c72f816ff88ea6daf32c6a20ac23cbd0939eea41
Sai Ramana Reddy <35768756+sairamanareddy@users.noreply.github.com>
2019-02-18 16:08:20 +0530

    Fix #21 : implementing handshake author@sairamanareddy  (#33)
    * Update download.go
    
    * Update download.go
    
    * Update download.go
    
    * FIx handshake issue
    
    * Update download.go
    
    * Update download.go
    
    * Included msghandler
    
    * Update msgHandler
    
    * Update onWholeMessage
    
    * msgHandler changes
    
    * Update download.go
    
    * Update issue#21 branch
    
    * Update download_test.go
    
    * Update download.go
    
    * Updated download.go and modified test for download.go
    
    Signed-off-by: Sai Ramana Reddy <cs17btech11022@iith.ac.in>
    
    * Write Tests for download.go and also update download.go
    
    Signed-off-by: Sai Ramana Reddy <cs17btech11022@iith.ac.in>
    
    * Update download.go
    
    * Update download.go
    
    Signed-off-by: Sai Ramana Reddy <cs17btech11022@iith.ac.in>

--- torrent/download.go
+++ torrent/download.go
@@ -4,9 +4,54 @@ import (
 	"bytes"
 	"encoding/binary"
 	"net"
+	"fmt"
+	"github.com/concurrency-8/tracker"
 )
 
-type handler func([]byte)
+type handler func([]byte , net.Conn) error
+
+
+// Download is a function that handshakes with a peer specified by peer object.
+// Concurrently call this function to establish parallel connections to many peers.
+func Download(peer tracker.Peer, report *tracker.ClientStatusReport) error{
+	buffer, err := BuildHandshake(*report)
+	if err!=nil{
+		return err
+	}
+    peerip := make([]byte, 4)
+    binary.BigEndian.PutUint32(peerip, peer.IPAdress)
+	service := net.TCPAddr{
+		IP: peerip,
+		Port: int(peer.Port),
+		Zone: "",
+	}
+	conn, err := net.Dial("tcp", service.String())
+	if err!=nil{
+		return err
+	}
+	//write the handshake content into the connection.
+	_, err = conn.Write(buffer.Bytes())
+	if err!=nil{
+		return err
+	}
+	//safely handle reading using onWholeMessage
+	onWholeMessage(conn, msgHandler)
+	return err
+}
+func msgHandler(msg []byte , conn net.Conn) error{
+
+	if (len(msg) == int(uint8(msg[0])) + 49) && (bytes.Equal(msg[1:20], []byte("BitTorrent protocol"))) {
+		message, err := BuildInterested()
+		if err!=nil{
+			fmt.Println("Error", err.Error())
+			return err
+		}
+		conn.Write(message.Bytes())
+	}
+	/* Other non-handshake functions should follow */
+	return nil
+	
+}
 
 // onWholeMessage sends complete messages to callback function
 func onWholeMessage(conn net.Conn, msgHandler handler) error {
@@ -16,9 +61,10 @@ func onWholeMessage(conn net.Conn, msgHandler handler) error {
 
 	for {
 		respLen, err := conn.Read(resp)
-
+		//Please look for a better connection handling in the future.
+		//Maybe use defer?
 		if err != nil {
-			conn.Close() // TODO maybe a better implementation
+			conn.Close()
 			return err
 		}
 
@@ -36,7 +82,7 @@ func onWholeMessage(conn net.Conn, msgHandler handler) error {
 		}
 		for len(buffer.Bytes()) >= 4 && len(buffer.Bytes()) >= msgLen {
 			// TODO implement msgHandler
-			msgHandler((buffer.Bytes())[:msgLen])
+			msgHandler((buffer.Bytes())[:msgLen] , conn)
 			buffer = bytes.NewBuffer((buffer.Bytes())[msgLen:])
 			handshake = false
 		}
