0f2a9cd98deff760d569abb99bb75a7030aa8a27
shraiysh <cs17btech11050@iith.ac.in>
2019-02-20 00:14:17 +0530

    Unnecessary (*tracker) changed to tracker

--- piece/piece.go
+++ piece/piece.go
@@ -16,12 +16,12 @@ type PieceTracker struct {
 // NewPieceTracker returns a new PieceTracker object for the torrent
 func NewPieceTracker(torrent parser.TorrentFile) (tracker *PieceTracker) {
 	tracker = new(PieceTracker)
-	(*tracker).Torrent = torrent
+	tracker.Torrent = torrent
 	numPieces := uint32(len(torrent.Piece) / 20)
 	for i := uint32(0); i < numPieces; i++ {
 		blocksPerPiece, _ := parser.BlocksPerPiece(torrent, i)
-		(*tracker).Requested = append((*tracker).Requested, make([]bool, blocksPerPiece))
-		(*tracker).Received = append((*tracker).Received, make([]bool, blocksPerPiece))
+		tracker.Requested = append(tracker.Requested, make([]bool, blocksPerPiece))
+		tracker.Received = append(tracker.Received, make([]bool, blocksPerPiece))
 	}
 	return
 }
@@ -30,22 +30,22 @@ func NewPieceTracker(torrent parser.TorrentFile) (tracker *PieceTracker) {
 // Invoked while requesting the block of a piece
 func (tracker *PieceTracker) AddRequested(block parser.PieceBlock) {
 	index := block.Begin / parser.BLOCK_LEN
-	(*tracker).Requested[block.Index][index] = true
+	tracker.Requested[block.Index][index] = true
 }
 
 // AddReceived flags the received value of a block in a piece
 // Invoked when a block is received
 func (tracker *PieceTracker) AddReceived(block parser.PieceBlock) {
 	index := block.Begin / parser.BLOCK_LEN
-	(*tracker).Received[block.Index][index] = true
+	tracker.Received[block.Index][index] = true
 }
 
 func (tracker *PieceTracker) setRequested(array [][]bool) {
-	(*tracker).Requested = array
+	tracker.Requested = array
 }
 
 func (tracker *PieceTracker) setReceived(array [][]bool) {
-	(*tracker).Received = array
+	tracker.Received = array
 }
 
 // Needed checks if we want a block. If we have already requested all,
@@ -54,7 +54,7 @@ func (tracker *PieceTracker) Needed(block parser.PieceBlock) bool {
 
 	// Check if all have been requested...
 	allRequested := true
-	for _, i := range (*tracker).Requested {
+	for _, i := range tracker.Requested {
 		if !allRequested {
 			break
 		}
@@ -65,10 +65,10 @@ func (tracker *PieceTracker) Needed(block parser.PieceBlock) bool {
 
 	// If yes, copy received into request...
 	if allRequested {
-		(*tracker).Requested = clone((*tracker).Received)
+		tracker.Requested = clone(tracker.Received)
 	}
 
-	return !(*tracker).Requested[block.Index][block.Begin/parser.BLOCK_LEN]
+	return !tracker.Requested[block.Index][block.Begin/parser.BLOCK_LEN]
 }
 
 // Deep clones 2-D bool array
@@ -86,7 +86,7 @@ func clone(array [][]bool) (result [][]bool) {
 // IsDone tells if the torrent file has been successfully received
 func (tracker *PieceTracker) IsDone() (result bool) {
 	result = true
-	for _, i := range (*tracker).Received {
+	for _, i := range tracker.Received {
 		for _, j := range i {
 			result = result && j
 		}
@@ -97,7 +97,7 @@ func (tracker *PieceTracker) IsDone() (result bool) {
 // PrintPercentageDone prints the percentage of download completed on the screen
 func (tracker *PieceTracker) PrintPercentageDone() {
 	downloaded, total := 0.0, 0
-	for _, i := range (*tracker).Received {
+	for _, i := range tracker.Received {
 		for _, j := range i {
 			total++
 			if j {
