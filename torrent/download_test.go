a56e3e5f2e03ae5db82d322e9b26d765125d8958
Sai Ramana Reddy <35768756+sairamanareddy@users.noreply.github.com>
2019-02-25 17:23:17 +0530

    Update download_test.go

--- torrent/download_test.go
+++ torrent/download_test.go
@@ -4,14 +4,16 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"math/rand"
+	"net"
+	"testing"
+	"sync"
+
 	"github.com/concurrency-8/parser"
 	"github.com/concurrency-8/piece"
 	"github.com/concurrency-8/queue"
 	"github.com/concurrency-8/tracker"
 	"github.com/stretchr/testify/assert"
-	"math/rand"
-	"net"
-	"testing"
 )
 
 // TestOnWholeMessage tests torrent/download.go : onWholeMessage(*kwargs)
@@ -105,32 +107,79 @@ func TestUnChokeHandler(t *testing.T) {
 }
 
 // TestRequestPiece tests function RequestPiece
-func TestRequestPiece(t *testing.T) {
+// func TestRequestPiece(t *testing.T) {
 
+// 	file, _ := parser.ParseFromFile(parser.GetTorrentFileList()[0])
+// 	pieces := piece.NewPieceTracker(file)
+// 	queue := queue.NewQueue(file)
+// 	queue.Choked = false
+// 	pieceBlock := parser.RandomPieceBlock(file)
+// 	queue.Enqueue(pieceBlock.Index)
+// 	length := queue.Length()
+// 	client, server := net.Pipe()
+// 	fmt.Println(pieceBlock)
+// 	go func() {
+// 		for i := 0; i < length; i++ {
+// 			RequestPiece(server, pieces, queue)
+// 		}
+// 		defer server.Close()
+// 	}()
+
+// 	for i := 0; i < length; i++ {
+// 		resp := make([]byte, 17)
+// 		respLen, _ := client.Read(resp)
+// 		assert.Equal(t, respLen, 17, "Full message not received")
+// 		size, id, payload := ParseMsg(bytes.NewBuffer(resp))
+// 		assert.Equal(t, size, int32(13), "Request: Size not equal")
+// 		assert.Equal(t, id, int8(6), "Request: Message ID different")
+// 		assert.Equal(t, uint32(payload["index"].(int32)), pieceBlock.Index, "Request: index field of payload not same")
+// 		assert.Equal(t, uint32(payload["begin"].(int32)), uint32(i)*parser.BLOCK_LEN, "Request: begin field of payload not same")
+// 	}
+// }
+
+func TestHaveHandler(t *testing.T) {
+	var flag sync.WaitGroup
+	flag.Add(1)
+	fmt.Println("Testing torrent/download.go : HaveHandler")
 	file, _ := parser.ParseFromFile(parser.GetTorrentFileList()[0])
 	pieces := piece.NewPieceTracker(file)
 	queue := queue.NewQueue(file)
 	queue.Choked = false
 	pieceBlock := parser.RandomPieceBlock(file)
-	queue.Enqueue(pieceBlock.Index)
-	length := queue.Length()
+	fmt.Println("pieceBlock.Index", pieceBlock.Index)
 	client, server := net.Pipe()
-	fmt.Println(pieceBlock)
-	go func() {
-		for i := 0; i < length; i++ {
-			RequestPiece(server, pieces, queue)
-		}
-		defer server.Close()
-	}()
+	actualsamplemsg, err := BuildHave(pieceBlock.Index)
+	assert.Nil(t, err, "error writing to Buffer in BuildHave")
+	go func(){
+		_, err = server.Write(actualsamplemsg.Bytes())
+		flag.Done()
 
-	for i := 0; i < length; i++ {
-		resp := make([]byte, 17)
-		respLen, _ := client.Read(resp)
-		assert.Equal(t, respLen, 17, "Full message not received")
-		size, id, payload := ParseMsg(bytes.NewBuffer(resp))
-		assert.Equal(t, size, int32(13), "Request: Size not equal")
-		assert.Equal(t, id, int8(6), "Request: Message ID different")
-		assert.Equal(t, uint32(payload["index"].(int32)), pieceBlock.Index, "Request: index field of payload not same")
-		assert.Equal(t, uint32(payload["begin"].(int32)), uint32(i)*parser.BLOCK_LEN, "Request: begin field of payload not same")
-	}
+	}()
+	buffer := new(bytes.Buffer)
+
+	resp := make([]byte, 10)
+	respLen, err := client.Read(resp)
+	flag.Wait()
+	flag.Add(1)
+	assert.Nil(t, err, "Error reading from pipe")
+	err = binary.Write(buffer, binary.BigEndian, resp[:respLen])
+	assert.Nil(t, err, "Error writing to buffer.")
+	size, id, payload := ParseMsg(buffer)
+	assert.Equal(t, int8(4), id, "Invalid id after reading from pipe.")
+	assert.Equal(t, int32(5), size, "Invalid size")
+	assert.NotEmpty(t, payload["payload"], "Empty piece index in payload.")
+	var pieceIndex uint32
+	go func(){
+		fmt.Println("Before calling HaveHandler, queue length=", queue.Length())
+		pieceIndex, err = HaveHandler(server, pieces, queue, payload)
+		assert.Nil(t, err, "Error in HaveHandler")
+		assert.Equal(t, pieceBlock.Index, pieceIndex, "Piece Index doesn't match.")
+		flag.Done()
+		server.Close()
+	}()
+	client.Read(resp)
+	flag.Wait()
+	fmt.Println(resp)
+	
+
 }
