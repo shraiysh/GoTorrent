d58a32d970a271f187509bedbdbefde27a967a26
Sai Ramana Reddy <cs17btech11022@iith.ac.in>
2019-02-27 20:57:50 +0530

    Update main.go
    Signed-off-by: Sai Ramana Reddy <cs17btech11022@iith.ac.in>

--- main.go
+++ main.go
@@ -2,31 +2,45 @@ package main
 
 import (
 	"fmt"
-
+	"bufio"
+	"os"
+	"io"
+	"strings"
+	// "github.com/concurrency-8/parser"
+	// "github.com/concurrency-8/piece"
 	"github.com/concurrency-8/torrent"
+	// "github.com/concurrency-8/tracker"
+	// "net/url"
 )
 
 func main() {
+	fmt.Printf("GoTorrent v1.0\nType \"help\" or \"license\" for more information.\n")
+	scanner := bufio.NewReader(os.Stdin)
+	ports := make([]int, 1)
+	ports[0] = 7000
+	var text string
+	var err error
+	text = ""
+	for text!="exit" && err!=io.EOF{
+		fmt.Printf(">>> ")
+		text, err = scanner.ReadString('\n')
+		switch {
+		case text=="help\n":
+			fmt.Println("\n\tUsage:")
+			fmt.Println("\t\tfile [path] -- Downloads the torrent file specified by path.")
+			fmt.Println("\t\tshow   -- Shows current files that are being downloaded.")
+			fmt.Println("\t\tpause [id]  -- Pauses the download of torrent specified by torrent index aka id.\n\t\t\t To show torrent index, use 'show'.")
+			fmt.Println("\t\tresume [id] -- Resumes the download of torrent specified by torrent index aka id.\n\t\t\t To show torrent index, use 'show'.")
+			fmt.Println("\t\texit -- Exit shell.")
+		case len(text)>4 && text[:4]=="file":
+			text := strings.TrimSpace(text[4:])
+			fmt.Println(text)
+			//Get the next port to download, maynot be available, download will fail with error.
+			next := ports[len(ports)-1] + 1
+			ports = append(ports, next)
+			go torrent.DownloadFromFile(text, next)
+		
+		}
 
-	torrent.DownloadFromFile("./test_torrents/Game of thrones - Spoils of war.torrent", 6881)
-	// torrentfile, _ := parser.ParseFromFile("./test_torrents/ubuntu.iso.torrent")
-	// fmt.Println(torrentfile.PieceLength)
-	// u, err := url.Parse(torrentfile.Announce[0])
-	// fmt.Println(u)
-	// if err != nil {
-	// 	return
-	// }
-
-	// clientReport := tracker.GetClientStatusReport(torrentfile, 6881)
-
-	// announce, err := tracker.GetPeers(u, clientReport)
-
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-
-	// pieces := piece.NewPieceTracker(torrentfile)
-	// torrent.Download(announce.Peers[1], clientReport, pieces)
-	fmt.Println("It's over")
+	}
 }
