45a51b53b644fb46e2423e58ee2e77835daa45ba
Sai Ramana Reddy <cs17btech11022@iith.ac.in>
2019-02-28 04:43:45 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,46 +1,43 @@
 package main
 
 import (
-	"fmt"
-	"bufio"
 	"os"
-	"io"
-	"strings"
-	// "github.com/concurrency-8/parser"
-	// "github.com/concurrency-8/piece"
-	"github.com/concurrency-8/torrent"
-	// "github.com/concurrency-8/tracker"
-	// "net/url"
+	"fmt"
 )
 
 func main() {
-	fmt.Printf("GoTorrent v1.0\nType \"help\" or \"license\" for more information.\n")
-	scanner := bufio.NewReader(os.Stdin)
-	ports := make([]int, 1)
-	ports[0] = 7000
-	var text string
-	var err error
-	text = ""
-	for text!="exit" && err!=io.EOF{
-		fmt.Printf(">>> ")
-		text, err = scanner.ReadString('\n')
-		switch {
-		case text=="help\n":
-			fmt.Println("\n\tUsage:")
-			fmt.Println("\t\tfile [path] -- Downloads the torrent file specified by path.")
-			fmt.Println("\t\tshow   -- Shows current files that are being downloaded.")
-			fmt.Println("\t\tpause [id]  -- Pauses the download of torrent specified by torrent index aka id.\n\t\t\t To show torrent index, use 'show'.")
-			fmt.Println("\t\tresume [id] -- Resumes the download of torrent specified by torrent index aka id.\n\t\t\t To show torrent index, use 'show'.")
-			fmt.Println("\t\texit -- Exit shell.")
-		case len(text)>4 && text[:4]=="file":
-			text := strings.TrimSpace(text[4:])
-			fmt.Println(text)
-			//Get the next port to download, maynot be available, download will fail with error.
-			next := ports[len(ports)-1] + 1
-			ports = append(ports, next)
-			go torrent.DownloadFromFile(text, next)
-		
-		}
-
+	var errormsg = `Usage of ./main:
+	--r
+		  True if pause and resume feature is needed. False otherwise.
+	--v
+		  True if misc output is required. False otherwise.
+	--files [path] [path] ...
+		  List of Torrent Files`
+	l := len(os.Args)
+	if l == 1{
+		fmt.Println(errormsg)
+		return
 	}
+	files := make([]string, 1)
+	var filesflag = false
+	var resumeflag = false
+	var verboseflag = false
+	for i:=1;i<l;i++{
+		arg := os.Args[i]
+		if filesflag==true && arg[0]!='-' {
+			files = append(files, arg)
+		} else if filesflag==true{
+			if arg=="--r" || arg=="-r" {
+				resumeflag = true
+			} else if arg=="--v" || arg=="-v"{
+				verboseflag = true
+			}
+		}
+		if arg == "--files"{
+			filesflag=true
+		}
+	}
+	
+
+
 }
