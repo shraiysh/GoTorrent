12335ae2ee6ed1bb0e020b9932430303cb4510ec
Puneet Mangla <cs17btech11029@iith.ac.in>
2019-03-01 12:34:07 +0530

    Circleci fix (#64)

--- tracker/utils.go
+++ tracker/utils.go
@@ -9,21 +9,23 @@ import (
 	"github.com/concurrency-8/parser"
 	bencode "github.com/zeebo/bencode"
 	"io/ioutil"
+	"log"
 	"math"
+	"math/rand"
 	"net"
 	"net/http"
 	"net/url"
 	"os"
 	"strconv"
 	"time"
-	"math/rand"
-	"log"
 )
- var (
-        root string
-        torrents []string
-        err error
-        )
+
+var (
+	root     string
+	torrents []string
+	err      error
+)
+
 // buildConnReq is the first connection request for tracker
 func buildConnReq() []byte {
 	var buffer bytes.Buffer
@@ -367,19 +369,19 @@ func GetClientStatusReport(torrent parser.TorrentFile, port uint16) (report *Cli
 
 	return
 }
-func GetRandomTorrent() (parser.TorrentFile) {
+func GetRandomTorrent() parser.TorrentFile {
 	root = "././test_torrents"
 	//Read the directory
-    files, err := ioutil.ReadDir(root)
-    if err != nil {
-        log.Fatal(err)
-    }
-    //store in the list
-    for _, f := range files {
-        torrents = append(torrents, f.Name())
-    }
-    rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
-    random_torrent:=root +"/" + torrents[rand.Intn(len(torrents))]
-    store, _ := parser.ParseFromFile(random_torrent)
-    return store
+	files, err := ioutil.ReadDir(root)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//store in the list
+	for _, f := range files {
+		torrents = append(torrents, f.Name())
+	}
+	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
+	random_torrent := root + "/" + torrents[rand.Intn(len(torrents))]
+	store, _ := parser.ParseFromFile(random_torrent)
+	return store
 }
